c469fc6015c3ee50e61539452f8b289eb8610184
Ben Ford <benjamin.ford@samsclub.com>
2025-04-24 13:56:08 -0700

    Removed auto command for color scheme

--- nvim/.config/nvim/lua/plugins/custom-commands.lua
+++ nvim/.config/nvim/lua/plugins/custom-commands.lua
@@ -9,14 +9,9 @@ return {
       vim.api.nvim_create_user_command("Gcan", function()
         vim.cmd("!git commit --amend --no-edit")
       end, {})
-
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "*",
-        callback = function()
-          vim.api.nvim_set_hl(0, "LineNr", { fg = "#FFFFFF", bg = "NONE" })
-          vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFA500", bold = true })
-        end,
-      })
     end,
+    opts = {
+      colorscheme = "tokyonight",
+    },
   },
 }

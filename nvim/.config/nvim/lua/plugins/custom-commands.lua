b1e0cc006b83b823ba70cea23fa1471572569dc4
Ben Ford <benjamin.ford@samsclub.com>
2025-04-24 09:39:22 -0700

    Added amend with verify

--- nvim/.config/nvim/lua/plugins/custom-commands.lua
+++ nvim/.config/nvim/lua/plugins/custom-commands.lua
@@ -5,6 +5,10 @@ return {
       vim.api.nvim_create_user_command("Gcann", function()
         vim.cmd("!git commit --amend --no-edit --no-verify")
       end, {})
+
+      vim.api.nvim_create_user_command("Gcan", function()
+        vim.cmd("!git commit --amend --no-edit")
+      end, {})
     end,
   },
 }

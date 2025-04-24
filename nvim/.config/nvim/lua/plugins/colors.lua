4b2171516af850843363ff2d3428c9faa73f5cd0
Ben Ford <benjamin.ford@samsclub.com>
2025-04-24 12:25:48 -0700

    trying moon

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,12 +1,10 @@
 return {
-  "LazyVim/LazyVim",
-  init = function()
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      pattern = "*",
-      callback = function()
-        vim.api.nvim_set_hl(0, "LineNr", { fg = "#FFAA00", bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFA500", bold = true })
-      end,
-    })
-  end,
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "moon", -- make sure this is set!
+    on_highlights = function(hl, c)
+      hl.LineNr = { fg = "#FFFFFF", bg = "NONE" }
+      hl.CursorLineNr = { fg = "#FFA500", bold = true }
+    end,
+  },
 }

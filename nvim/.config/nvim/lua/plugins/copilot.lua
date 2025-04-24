6472a87424ce44c1aa44c1b30deb61e6c8f76b2d
Ben Ford <benjamin.ford@samsclub.com>
2025-04-24 09:39:38 -0700

    Added copilot

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,52 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot", -- lazy-load on command
+  event = "VeryLazy", -- or use "VeryLazy" for even more lazy loading
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-j>",
+          prev = "<C-k>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        help = false,
+        gitcommit = true,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["*"] = true,
+      },
+      copilot_node_command = "node", -- Ensure Node.js is installed
+      server_opts_overrides = {
+        auth_provider_url = "https://gecgithub01.walmart.com",
+      },
+    })
+  end,
+}

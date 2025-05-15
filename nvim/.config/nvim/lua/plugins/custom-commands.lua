a61b9e34a86f95e14ca489e75ac1870242b55d6c
Ben Ford <bentford@gmail.com>
2025-05-15 08:21:26 -0700

    Added two options for a dice roll

--- nvim/.config/nvim/lua/plugins/custom-commands.lua
+++ nvim/.config/nvim/lua/plugins/custom-commands.lua
@@ -9,6 +9,17 @@ return {
       vim.api.nvim_create_user_command("Gcan", function()
         vim.cmd("!git commit --amend --no-edit")
       end, {})
+
+      vim.api.nvim_create_user_command("RandomLine", function()
+        local line_count = vim.api.nvim_buf_line_count(0)
+        local random_line = math.random(1, line_count)
+
+        -- Move to the line and enter visual line mode
+        vim.api.nvim_win_set_cursor(0, { random_line, 0 })
+        vim.cmd("normal! V")
+
+        print("Selected line: " .. random_line)
+      end, {})
     end,
   },
 }

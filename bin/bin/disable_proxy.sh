b341b06b0fdb99b49102978a87fa080b050a1d40
Ben Ford <bentford@gmail.com>
2025-05-15 08:21:08 -0700

    Move individual scripts to one

--- bin/bin/disable_proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/osascript
-
-set networkService to "Wi-Fi" -- Replace with your network service name if different
-do shell script "networksetup -setproxyautodiscovery " & quoted form of networkService & " off"
-
-return "automatic proxy disabled"
--- bin/bin/launch_network_apps.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/osascript
-
-tell application "Proxyman" to activate
-tell application "Cisco Secure Client" to activate

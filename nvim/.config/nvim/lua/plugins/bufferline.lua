41e4b78ff9e5cf2e3df30a2ffadb4780f4a5eddb
Ben Ford <bentford@gmail.com>
2025-05-27 11:05:15 -0700

    Added move buffer keyboard shortcuts

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<A-h>", "<cmd>BufferLineMovePrev<CR>", desc = "Move buffer left" },
+    { "<A-l>", "<cmd>BufferLineMoveNext<CR>", desc = "Move buffer right" },
+  },
+}

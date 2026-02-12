454cfd48485d887cb71b471abef77a54d44c4e8c
Ben Ford <ben@fordcrossing.com>
2026-02-12 06:39:17 -0800

    Add sim log debug shortcut

--- nvim/.config/nvim/lua/plugins/custom-commands.lua
+++ nvim/.config/nvim/lua/plugins/custom-commands.lua
@@ -43,6 +43,29 @@ return {
         print("Selected line: " .. random_line)
       end, {})
 
+      local function stream_sim_logs()
+        local bundle_id = vim.g.sim_log_bundle_id
+        if not bundle_id or bundle_id == "" then
+          vim.ui.input({ prompt = "Bundle identifier: " }, function(input)
+            if not input or input == "" then
+              return
+            end
+            vim.g.sim_log_bundle_id = input
+            stream_sim_logs()
+          end)
+          return
+        end
+
+        local cmd = string.format(
+          "xcrun simctl spawn booted log stream --level=debug --predicate 'subsystem=\"%s\"' --style compact",
+          bundle_id
+        )
+        vim.cmd("tabnew | terminal " .. cmd)
+      end
+
+      vim.api.nvim_create_user_command("SimLog", stream_sim_logs, {})
+      vim.keymap.set("n", "<leader>xS", stream_sim_logs, { desc = "Stream simulator logs" })
+
       vim.api.nvim_create_user_command("NewDaily", function(opts)
         local date = os.date("%Y-%m-%d")
         local filename = date .. ".md"
